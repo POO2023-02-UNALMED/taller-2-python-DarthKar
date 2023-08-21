d489be5e614d6191a1b3559c8009fa5cb38d3506
Miguel Angel Peña Marin <138605407+DarthKar@users.noreply.github.com>
2023-08-20 19:19:10 -0500

    Todo de una Main.py

---
+++ main.py
@@ -0,0 +1,48 @@
+
+class Asiento:
+    def __init__(self, color, registro, precio,):
+        self.color = color
+        self.registro = registro
+        self.precio = precio    
+    def cambiarColor(self, color):
+       if color == "rojo" or color=="verde" or color=="amarillo" or color=="negro" or color=="blanco": 
+            self.color = color
+class Motor:
+    def __init__(self, numeroCilindros, tipo, registro):
+        self.numeroCilindros = numeroCilindros
+        self.tipo = tipo
+        self.registro = registro
+    def cambiarRegistro(self, registro):
+        self.registro = registro
+    def asignarTipo(self, tipo):
+        if tipo == "electrico" or tipo == "gasolina":
+            self.tipo = tipo
+class Auto:
+    def __init__(self, modelo, precio, asientos, marca, motor, registro, cantidadCreados):
+        self.modelo = modelo
+        self.precio = precio
+        self.asientos = asientos
+        self.marca = marca
+        self.motor = motor
+        self.registro = registro
+        self.cantidadCreados = cantidadCreados
+    def cantidadAsientos(self):
+        nAsientos=0
+        for i in range(self.asientos.length):
+            if self.asientos[i] != None:
+                nAsientos+=1
+            return nAsientos
+    def verificarIntegridad(self):
+        if self.registro == Motor.registro:
+            for i in range(self.asientos.length):
+                if self.asientos[i] != None:
+                    if self.asientos[i].registro != self.registro:
+                        return "Las piezas no son originales"
+            return "Auto original"
+        else:
+            return "Las piezas no son originales"
+
+
+        
+
+
